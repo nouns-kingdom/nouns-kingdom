991e91be1048e1a1573d44f40b504a285c8b534c
princetonhuaren <axgong@outlook.com>
2025-08-24 15:02:45 -0400

    rewrite with react

---
+++ react-app/src/services/httpService.js
@@ -0,0 +1,28 @@
+class HttpServiceClient {
+  Categories() {
+    return fetch("./db_category.JSON");
+    // return ["animals", "foods", "tools"];
+  }
+
+  WordsOfCard(category, card) {
+    const dictionay = "/db_" + category + ".JSON";
+    console.log("dictionay", dictionay, card);
+    return fetch(dictionay)
+      .then((res) => res.json())
+      .then((data) => data[card]);
+    // .then((res) => console.log(res))
+    // .then((data) => {
+    //   console.log("Available keys:", Object.keys(data));
+    //   console.log("Requested card:", card);
+    //   return data[card];
+    // })
+    // return fetch(dictionay);
+  }
+
+  WordsOfCategory(category) {
+    const curCategory = "/db_" + category + ".JSON";
+    return fetch(curCategory);
+  }
+}
+
+export const NKHttpSvc = new HttpServiceClient();
---
+++ react-app/src/views/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { NKHttpSvc } from "../services/httpService";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    NKHttpSvc.Categories()
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data.category);
+        setIsLoading(false);
+        console.log("home data", data);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const getImgUrl = (name) => `/assets/category/${name}.png`;
+  const onCardChange = (category) => navigate(`/${category}`);
+
+  return (
+    <div className='category-container' style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", marginLeft: "15%", marginRight: "15%" }}>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : categories.length > 0 ? (
+        categories.map((category) => (
+          <div key={category} style={{ margin: "1em" }}>
+            {/* <div>{getImgUrl(category)}</div> */}
+            <img src={getImgUrl(category)} alt={category} className='image-container' style={{ height: "300px", width: "280px", cursor: "pointer" }} onClick={() => onCardChange(category)} />
+            <div className='card-font-container' style={{ fontWeight: 600, fontSize: "2vw", cursor: "pointer" }} onClick={() => onCardChange(category)}>
+              {category}
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>Will add more categories here soon</div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
